236d48cbca29a02fa1dc0db70bc13f5201fdfeab
boyan <boyan@localhost>
2018-12-31 15:48:09 +0200

    Initial Commit of Helper Class

---
+++ DataStructuresExcersises/src/utililities/Helper.java
@@ -0,0 +1,13 @@
+package utililities;
+
+public class Helper {
+
+	public static void main(String[] args) {
+		Helper.testStart();
+	}
+	
+	public static void testStart() {
+		System.out.print("It Works");
+	}
+
+}
